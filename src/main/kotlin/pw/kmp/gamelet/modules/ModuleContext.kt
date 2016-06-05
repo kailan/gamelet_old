6565291e952889b796d23932a58559ae95adab3e
Kailan Blanks <kailan@kmp.pw>
2016-06-05 23:14:25 +0100

    test

--- src/main/kotlin/pw/kmp/gamelet/modules/ModuleContext.kt
+++ src/main/kotlin/pw/kmp/gamelet/modules/ModuleContext.kt
@@ -17,11 +17,16 @@ class ModuleContext(val match: Match) {
     }
 
     @Suppress("UNCHECKED_CAST")
+    fun <T : Module> getFactory(module: KClass<T>) : ModuleFactory<T>? {
+        return ModuleRegistry.modules.filter { it.getModuleClass() == module }.firstOrNull() as ModuleFactory<T>
+    }
+
+    @Suppress("UNCHECKED_CAST")
     fun <T : Module> getModule(module: KClass<T>): T? {
         return modules.find { it.javaClass == module.java } as T
     }
 
-    operator fun <T : Module> get(module: KClass<T>): T = getModule(module)!!
+    operator fun <T : Module> get(module: KClass<T>): T = getModule(module) ?: throw IllegalArgumentException("Tried to access unloaded module")
 
     fun loadModule(factory: ModuleFactory<Module>): Boolean {
         if (hasModule(factory.getModuleClass())) return true
@@ -37,7 +42,7 @@ class ModuleContext(val match: Match) {
 
     fun depend(vararg modules: KClass<out Module>) {
         modules.forEach { module ->
-            val factory = ModuleRegistry.modules.filter { it.getModuleClass() == module }.firstOrNull() ?: throw DependencyException(module)
+            val factory = getFactory(module) ?: throw DependencyException(module)
             if (!loadModule(factory)) throw DependencyException(module)
         }
     }
